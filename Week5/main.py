1e56ed668e823fee85da419d3a2e259b0ded8fbb
Bjarke Heldbo <bjarke@heldbo.net>
2022-03-13 21:34:07 +0100

    Add slice indexes for target x,y

--- Week5/main.py
+++ Week5/main.py
@@ -7,14 +7,20 @@ from sklearn.neighbors import KNeighborsClassifier
 from scipy.spatial import procrustes
 
 def setup():
-    X_train = np.loadtxt('diatoms/SIPdiatomsTrain.txt', delimiter = ',')
-    X_test = np.loadtxt('diatoms/SIPdiatomsTest.txt', delimiter = ',')
-    X_train_classes = np.loadtxt('diatoms/SIPdiatomsTrain_classes.txt', delimiter = ',')
-    X_test_classes = np.loadtxt('diatoms/SIPdiatomsTest_classes.txt', delimiter = ',')
+    X_train = np.loadtxt('diatom/SIPdiatomsTrain.txt', delimiter = ',')
+    X_test = np.loadtxt('diatom/SIPdiatomsTest.txt', delimiter = ',')
+    X_train_classes = np.loadtxt('diatom/SIPdiatomsTrain_classes.txt', delimiter = ',')
+    X_test_classes = np.loadtxt('diatom/SIPdiatomsTest_classes.txt', delimiter = ',')
     return X_train, X_test, X_train_classes, X_test_classes
 
 def task2_1():
-    pass
+    X_train, X_test, X_train_y, X_test_y = setup()
+    target_x =  X_train[0, ::2] 
+    target_y = X_train[0, 1::2]
 
 if __name__ == '__main__':
     X_train, X_test, X_train_y, X_test_y = setup()
+    print(X_train[0],X_train[0,::2],X_train[0,1::2])
+    plt.scatter(X_train[0,::2],X_train[0,1::2])
+    task2_1()
+    plt.show()
