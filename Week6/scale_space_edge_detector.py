a81578dbcaba7f7ee9c08b16b3ba1a8c5fd59c36
Bjarke Heldbo <bjarke@heldbo.net>
2022-03-21 11:02:16 +0100

    task 3.1

--- Week6/scale_space_edge_detector.py
+++ Week6/scale_space_edge_detector.py
@@ -1,6 +1,19 @@
-def task3_1():
-    pass
+import numpy as np
+import matplotlib.pyplot as plt
+
+def task3_1(sigma=0.1):
+    def _soft_edge(y, x, sigma=2):
+        x = (x - ((len(x) - 1) / 2))
+        left = 1 / (np.sqrt(2 * np.pi * sigma ** 2))
+        right = np.exp(-((x ** 2) / (2 * sigma ** 2)))
+        return left * right
 
 
+    points = np.fromfunction(_soft_edge, (16, 16), dtype=float)
+    print(points)
+    img = np.cumsum(points, axis=1)
+    plt.imshow(img, cmap='gray')
 
 if __name__ == "__main__":
+    task3_1()
+    plt.show()
